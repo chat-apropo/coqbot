99ea6cae2c6b0f2d0fca502abca0a85c8da8d4e6
Matheus Fillipe <matheusfillipeag@gmail.com>
2022-05-29 16:35:21 +0000

    paste and read from url

--- bot.py
+++ bot.py
@@ -1,17 +1,20 @@
 import re
-import trio
 import signal
-from pexpect import replwrap
-from cachetools import TTLCache
-from tempfile import NamedTemporaryFile
 import threading
+from tempfile import NamedTemporaryFile
 
+import requests
+import trio
+from cachetools import TTLCache
 from IrcBot.bot import IrcBot, Message, utils
+from pexpect import replwrap
 
-from conf import CHANNELS, NICK, SERVER, PORT, PREFIX, COQTOP_CMD, SSL, COQ_REPL_TTL
+from conf import (CHANNELS, COQ_REPL_TTL, COQTOP_CMD, NICK, PORT, PREFIX,
+                  SERVER, SSL)
 from message_server import listen_loop
 
-user_repls = TTLCache(maxsize=32, ttl=COQ_REPL_TTL)
+user_repls = TTLCache(maxsize=5, ttl=COQ_REPL_TTL)
+user_history = TTLCache(maxsize=32, ttl=COQ_REPL_TTL)
 
 utils.setHelpHeader(
     "USE: {PREFIX} [coq command here]       - (Notice the space)")
@@ -44,11 +47,27 @@ def reply(msg: Message, text: str):
             line = f"<{msg.nick}> {ansi2irc(line)}"
             f.write(f"[[{msg.channel}]] {line}\n")
 
+
+def paste(text):
+    """Paste text to ix.io."""
+    info(f"Pasting {text=}")
+    url = "http://ix.io"
+    payload = {'f:1=<-': text}
+    response = requests.request("POST", url, data=payload)
+    return response.text
+
+
+def read_paste(url):
+    """Read text from ix.io."""
+    response = requests.request("GET", url)
+    return response.text
+
+
 def run_command(msg: Message, text: str):
 
     def _run_command(msg: Message, text: str):
         def __run_command(msg: Message, text: str):
-            global user_repls
+            global user_repls, user_history
             info(f"{user_repls=}")
             user = msg.nick
             if user not in user_repls:
@@ -58,7 +77,12 @@ def run_command(msg: Message, text: str):
             info(f"Running command for {user}")
             reply(msg, user_repls[user].run_command(text, timeout=2))
 
-        t = threading.Thread(target=__run_command, args=(msg, text), daemon=True)
+            if user not in user_history:
+                user_history[user] = []
+            user_history[user].append(text)
+
+        t = threading.Thread(target=__run_command,
+                             args=(msg, text), daemon=True)
         t.start()
         t.join(2)
         if t.is_alive():
@@ -83,6 +107,26 @@ async def run(bot: IrcBot, match: re.Match, message: Message):
     run_command(message, text)
 
 
+@utils.arg_command("paste", "Pastes your environment code")
+async def pipaste(bot: IrcBot, args: re.Match, msg: Message):
+    if msg.nick not in user_repls:
+        reply(msg, "You don't have an environment")
+        return
+    reply(msg, paste("\n".join(user_history[msg.nick])))
+
+
+@utils.arg_command("read", "Populates your environment code with code from url")
+async def readurl(bot: IrcBot, args: re.Match, msg: Message):
+    if not args[1]:
+        reply(msg, "Please provide a url")
+        return
+    try:
+        run_command(msg, read_paste(args[1]))
+    except Exception as e:
+        reply(msg, "Failed to read paste: " + str(e))
+    reply(msg, "Code has been read and sent!")
+
+
 async def onConnect(bot: IrcBot):
     for channel in CHANNELS:
         await bot.join(channel)
@@ -99,6 +143,7 @@ async def onConnect(bot: IrcBot):
         while True:
             global user_repls
             user_repls.pop(None, None)
+            user_history.pop(None, None)
             await trio.sleep(10)
 
     async with trio.open_nursery() as nursery:
